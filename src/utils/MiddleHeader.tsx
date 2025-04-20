5ddebc1d44e5f32530da611fb6caf67d0d041d3b
Pedro Regis <pedromr2@al.insper.edu.br>
2025-04-20 20:02:43 -0300

    Header

---
+++ src/utils/MiddleHeader.tsx
@@ -0,0 +1,113 @@
+import styled from "styled-components"
+import { ShoppingCart, User, MagnifyingGlass } from "@phosphor-icons/react"
+import { Input } from "@/components/ui/input"
+import logo from "@/assets/logo_loja.jpg"
+
+
+const MiddleHeader = () => {
+  return (
+
+    <Header>
+        <LogoLoja>
+          <a href="/">
+            <img src={logo} alt="Logo" />
+          </a>
+        </LogoLoja>
+
+        <BarraPesquisa>
+            <StyledInput type="text" placeholder="Buscar por"></StyledInput>
+            <IconePesquisa>
+                <MagnifyingGlass size={18} weight="regular" />
+            </IconePesquisa>
+        </BarraPesquisa>
+
+        <Icones>
+          <a href="/carrinho">
+            <button>
+              <ShoppingCart size={32} weight="regular" />
+            </button>
+          </a>
+          <a href="/login">
+            <button>
+              <User size={32} weight="regular" />
+            </button>
+          </a>
+        </Icones>
+    </Header>
+
+    
+    )
+}
+
+
+export default MiddleHeader
+
+const Header = styled.div`
+  width: 100%;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: white;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+`
+
+const LogoLoja = styled.button`
+  background: none;
+  border: none;
+  img {
+    height: 3rem;
+  }
+`
+
+const BarraPesquisa = styled.div`
+  position: relative;
+  flex: 1;
+  max-width: 500px;
+`
+
+const StyledInput = styled(Input)`
+  width: 100%;
+  padding: 0.5rem 1.5rem;
+  font-size: 0.875rem;
+  border-radius: 9999px;
+  background-color: #f2f2f2;
+  cursos: pointer;
+  border: 2px solid transparent;
+  padding-right: 0rem;
+  transition: border-color 0.2s ease-in-out;
+  
+  &:hover {
+    border-color:black;
+  }
+`
+
+const IconePesquisa = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 0rem;
+  transform: translateY(-50%);
+  color: #555;
+  background: none;
+  border: none;
+`
+
+const Icones = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    padding: 0.4rem;
+    border-radius: 9999px;
+    transition: background-color 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #023e8a;
+      color: white;
+    }
+  }
+`
---
+++ src/utils/TopHeader.tsx
@@ -0,0 +1,91 @@
+import styled from "styled-components"
+import { WhatsappLogo, Phone } from "@phosphor-icons/react"
+
+const TopHeader = () => {
+  return (
+    <Header>
+        <LeftSection>
+          <InfoItem>
+            <WhatsappLogo size={24} weight="regular" color="white" />
+            <Contato>
+              <p>
+                Contate via WhatsApp: <br />
+                (11) 99999-9999 
+              </p>
+            </Contato>
+          </InfoItem>
+
+          <Divisor />
+
+          <InfoItem>
+            <Phone size={24} weight="regular" color="white" />
+            <Contato>
+              <p>
+                Televendas: <br />
+                (11) 0000-0000
+              </p>
+            </Contato>
+          </InfoItem>
+        </LeftSection>
+
+        <RightSection>
+          <LinkItem href="nossa-loja">Nossa loja</LinkItem>
+          <LinkItem href="atendimento">Atendimento</LinkItem>
+        </RightSection>
+    </Header>
+  )
+}
+
+export default TopHeader
+
+const Header = styled.header`
+  width: 100%;
+  padding: 0.25rem 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  font-family: Arial, sans-serif;
+  background-color: #023e8a;
+  color: white;
+  box-sizing: border-box;
+`
+
+const LeftSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+`
+
+const RightSection = styled.div`
+  display: flex;
+  gap: 2rem;
+`
+
+const InfoItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`
+
+const Contato = styled.div`
+  display: flex;
+  font-size: 0.75rem;
+`
+
+const Divisor = styled.div`
+  height: 2rem;
+  width: 2px;
+  background-color: white;
+`
+
+const LinkItem = styled.a`
+  font-size: 0.8rem;
+  color: white;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
